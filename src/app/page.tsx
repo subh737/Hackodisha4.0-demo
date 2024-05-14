08d44c90145705d8d447a920f454187c383786d8
Sanket <sanketyelave11@gmail.com>
2024-05-14 11:24:03 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-center font-bold text-[1.4rem] md:text-[4rem] sm:text-[2.5rem] mt-[4rem]">Hackodisha <span className="text-[purple]">4.0</span> Practice </h1>
+    </main>
+  );
+}
---
+++ src/app/prototype/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function page() {
+    return (
+        <div>
+            <h1 className="text-center font-bold text-[1.4rem] md:text-[4rem] sm:text-[2.5rem] mt-[4rem] text-[purple]">Hackodisha <span className="text-[black]">4.0</span> Practice </h1>
+
+        </div>
+    );
+}
+
+export default page;
