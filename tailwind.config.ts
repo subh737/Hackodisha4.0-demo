800aa27364ad4ce9aae7f6ac9b6a069d58d18d7c
Subham Pradhan <148049969+subh737@users.noreply.github.com>
2024-05-26 14:12:37 +0530

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        gvonz: ['titlefont', "monospacce"],
+        syoog: ['syoog', "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
